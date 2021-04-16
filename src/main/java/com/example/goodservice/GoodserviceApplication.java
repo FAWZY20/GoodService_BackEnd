79ac0eb5baf0d6f415a5373b8695638d4c612e42
Fawzy20 <fawzy-elsam@outlook.fr>
2021-04-16 11:47:02 +0200

    add file v02

--- src/main/java/com/example/goodservice/GoodserviceApplication.java
+++ src/main/java/com/example/goodservice/GoodserviceApplication.java
@@ -12,7 +12,6 @@ public class GoodserviceApplication {
         SpringApplication.run(GoodserviceApplication.class, args);
     }
 
-
     protected SpringApplicationBuilder configure(
             SpringApplicationBuilder builder) {
         return builder.sources(GoodserviceApplication.class);

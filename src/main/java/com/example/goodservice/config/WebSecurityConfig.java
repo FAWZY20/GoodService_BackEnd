de25053ca925b8a2a26ae94fb208bc3e1a5fe956
Fawzy20 <fawzy-elsam@outlook.fr>
2021-04-16 16:04:47 +0200

    dernier commit

--- src/main/java/com/example/goodservice/config/WebSecurityConfig.java
+++ src/main/java/com/example/goodservice/config/WebSecurityConfig.java
@@ -44,7 +44,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200", "Access-Control-Allow-Origin: https://mugiwara.csid.agilitejoviale.fr");
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200", "https://mugiwara.csid.agilitejoviale.fr");
             }
         };
     }

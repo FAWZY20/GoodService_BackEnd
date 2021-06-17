d9182cdf924d434d022ebe4bcd7c1cf5968aaef8
Fawzy20 <fawzy-elsam@outlook.fr>
2021-06-18 01:40:04 +0200

    reparation probleme liquibase path

--- src/main/java/com/example/goodservice/controller/ReservationRestController.java
+++ src/main/java/com/example/goodservice/controller/ReservationRestController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
 //@RequestMapping(path = "/api/reservation")
 public class ReservationRestController {
 
